008b1e0d7e49b321954070325935d824df937f4c
K-Katsumura <keiya.katsumura@gmail.com>
2023-06-25 15:10:24 +0900

    complete the header component and make it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default function Header() {
+  const [pageState, setPageState] = useState("ログイン")
   const location = useLocation();
   const navigate = useNavigate();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if(user){
+        setPageState("アカウント")
+      }　else {
+        setPageState("ログイン")
+      }
+    })
+  })
+  const auth = getAuth();
 
   function pathMatchRoute(route) {
     if (route === location.pathname) {
@@ -20,7 +32,8 @@ export default function Header() {
           <ul className='flex space-x-10'>
             <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && '!text-black !border-b-red-500'}`} onClick={()=>navigate("/")}>Home</li>
             <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && '!text-black !border-b-red-500'}`} onClick={()=>navigate("/offers")}>Offers</li>
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/sign-in") && '!text-black !border-b-red-500'}`} onClick={()=>navigate("/sign-in")}>Sign in</li>
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+              (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && '!text-black !border-b-red-500'}`} onClick={()=>navigate("/profile")}>{pageState}</li>
           </ul>
         </div>
       </header>
