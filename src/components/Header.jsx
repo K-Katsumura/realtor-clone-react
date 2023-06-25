187f4f202a9ff7445222f96dcc3568d459cd0277
K-Katsumura <keiya.katsumura@gmail.com>
2023-06-25 15:44:50 +0900

    create the spinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
     }
   }
   return (
-    <div className='bg-white border-b shadow-sm sticky top-0' z-50>
+    <div className='bg-white border-b shadow-sm sticky top-0' z-40>
       <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
         <div>
           <img src='https://placehold.jp/500x150.png' alt='logo' className='h-8 cursor-pointer' onClick={()=>navigate("/")}/>
