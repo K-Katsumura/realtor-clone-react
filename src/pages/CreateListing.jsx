b0f14c0bbd2a7744f0cb75c518f4e4bbcb4bac3e
K-Katsumura <keiya.katsumura@gmail.com>
2024-02-06 22:52:04 +0900

    ver.2

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -71,7 +71,7 @@ export default function CreateListing() {
         setLoading(true);
         if (+DiscountedPrice >= +regularPrice) {
             setLoading(false);
-            toast.error("ディスカウントプライスは通常価格より低く設定してな！これがver1")
+            toast.error("ディスカウントプライスは通常価格より低く設定してな！これがver2")
             return
         }
         if (images.length > 6) {
